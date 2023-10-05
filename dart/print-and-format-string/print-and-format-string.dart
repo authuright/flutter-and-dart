9acb11044eae65e0ebbada3b2d459bb605999a5f
Ninz <ninz@Ninzs-MacBook-Pro.local>
2023-10-05 21:47:56 +0700

    continue dart

--- dart/print-and-format-string/print-and-format-string.dart
+++ dart/print-and-format-string/print-and-format-string.dart
@@ -1,3 +1,15 @@
 void main() {
   print("Hello Dart");
+
+  // formating string dart
+  var name = "Jonh Doe";
+  print("My name is ${name}");
+  print("My name is $name");
+  var l = 5;
+  var b = 6;
+  print("The total of L and B is ${l + b}");
+
+  var first_name = "Sally";
+  var last_name = "Doe";
+  print("Your FirstName is $first_name and your LastName is $last_name");
 }
